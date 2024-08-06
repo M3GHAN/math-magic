4f33225d1a9c5e68abd838e483e4bdfb56318ce3
M3GHAN <meghan111117@gmail.com>
2024-08-06 15:41:41 +0530

    updated

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-from 
+from scripts import addition
+from scripts import division, multiplication, floordivision
+
+
+myaddition=addition.addition(1,5)
+print(myaddition)
+
+mydivision=division.divide_numbers(10,2)
+print(mydivision)
+
+myfloor=floordivision.floor_division(10,2)
+print(myfloor)
+
+mymulti=multiplication.multiply_numbers(10,2)
+print(mymulti)
